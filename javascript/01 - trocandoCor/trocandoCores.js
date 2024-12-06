fcef2c240b4cee491de1de9e95acc03fd043de75
Lucas <lucasg16809@gmail.com>
2024-12-06 15:52:00 -0300

    atualização das atividades

---
+++ javascript/01 - trocandoCor/trocandoCores.js
@@ -0,0 +1,26 @@
+// Criando uma função para alterar a cor
+function aplicarCor() {
+    const cor = document.getElementById('selecaoCor').value;
+
+    document.body.style.backgroundColor = cor;
+}
+
+// função para gerar uma cor aleatória
+function corAleatoria() {
+    document.getElementById("r").innerHTML = "R:";
+    document.getElementById("g").innerHTML = "G:";
+    document.getElementById("b").innerHTML = "B:";
+
+    // Função Math.random gera um número aleatório entre 0 e 0.999
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    document.getElementById("r").innerHTML += r;
+    document.getElementById("g").innerHTML += g;
+    document.getElementById("b").innerHTML += b;
+
+    const cor = `rgb(${r}, ${g}, ${b})`
+
+    document.body.style.backgroundColor = cor;
+}
